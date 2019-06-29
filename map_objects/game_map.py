1746520878d673cdcc3355ebf0b82740cefbdd07
Julio <julioevm@gmail.com>
2019-06-29 18:11:37 +0200

    Add enemy generation and turn mechanic.

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -1,5 +1,7 @@
+import tcod as libtcod
 from random import randint
 
+from entity import Entity
 from map_objects.rectangle import Rect
 from map_objects.tile import Tile
 
@@ -16,7 +18,8 @@ class GameMap:
 
         return tiles
 
-    def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player):
+    def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities, 
+                max_monsters_per_room):
         rooms = []
         num_rooms = 0
 
@@ -25,8 +28,8 @@ class GameMap:
             w = randint(room_min_size, room_max_size)
             h = randint(room_min_size, room_max_size)
             # Random position without going out of bounds
-            x = randint(0, map_width -w -1)
-            y = randint(0, map_height -h -1)
+            x = randint(0, map_width - w - 1)
+            y = randint(0, map_height - h - 1)
 
             new_room = Rect(x, y, w, h)
 
@@ -50,7 +53,7 @@ class GameMap:
 
                     (prev_x, prev_y) = rooms[num_rooms - 1].center()
                     # Randomly choose tunnel order
-                    if randint(0,1) == 1:
+                    if randint(0, 1) == 1:
                         # First move horizontally then vertically
                         self.create_h_tunnel(prev_x, new_x, prev_y)
                         self.create_v_tunnel(prev_y, new_y, new_x)
@@ -58,14 +61,16 @@ class GameMap:
                         # Else move vertically then horizontally
                         self.create_v_tunnel(prev_y, new_y, prev_x)
                         self.create_h_tunnel(prev_x, new_x, new_y)
-                
+
+                # Generate monsters in the room
+                self.place_entities(new_room, entities, max_monsters_per_room)
                 # Finally, append the new toom to the list
                 rooms.append(new_room)
                 num_rooms += 1
 
     def create_room(self, room):
         # Go though the tiles in the rectangle and make them passable
-        for x in range(room.x1 +1, room.x2):
+        for x in range(room.x1 + 1, room.x2):
             for y in range(room.y1 + 1, room.y2):
                 self.tiles[x][y].blocked = False
                 self.tiles[x][y].block_sight = False
@@ -76,9 +81,26 @@ class GameMap:
             self.tiles[x][y].block_sight = False
 
     def create_v_tunnel(self, y1, y2, x):
-            for y in range(min(y1, y2), max(y1, y2) + 1):
-                self.tiles[x][y].blocked = False
-                self.tiles[x][y].block_sight = False
+        for y in range(min(y1, y2), max(y1, y2) + 1):
+            self.tiles[x][y].blocked = False
+            self.tiles[x][y].block_sight = False
+
+    def place_entities(self, room, entities, max_monsters_per_room):
+        # Generate random number of monsters
+        number_of_monsters = randint(0, max_monsters_per_room)
+
+        for i in range(number_of_monsters):
+            # Choose a random location in the room
+            x = randint(room.x1 + 1, room.x2 - 1)
+            y = randint(room.y1 + 1, room.y2 - 1)
+
+            if not any([entity for entity in entities if entity.x == x and entity.y == y]):
+                if randint(0, 100) < 80:
+                    monster = Entity(x, y, 'o', libtcod.desaturated_green, 'Orc', blocks=True)
+                else:
+                    monster = Entity(x, y, 'T', libtcod.darker_green, 'Troll', blocks=True)
+
+                entities.append(monster)
 
     def is_blocked(self, x, y):
         if self.tiles[x][y].blocked:
