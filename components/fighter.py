b12a98e94e0ae441e626f4380478de99356981d2
Julio <julioevm@gmail.com>
2019-07-21 11:27:28 +0200

    Add message reporting, basic hud and death.

--- components/fighter.py
+++ components/fighter.py
@@ -8,13 +8,26 @@ class Fighter:
         self.power = power
 
     def take_damage(self, amount):
+        results = []
+
         self.hp -= amount
 
+        if self.hp <= 0:
+            results.append({'dead': self.owner})
+
+        return results
+
     def attack(self, target):
+
+        results = []
         damage = self.power - target.fighter.defense
 
         if damage > 0:
-            target.fighter.take_damage(damage)
-            print('{0} attacks {1} dealing {2} damage.'.format(self.owner.name.capitalize(), target.name.capitalize(), damage))
+            results.append({'message': '{0} attacks {1} for {2} hit points.'.format(
+                self.owner.name.capitalize(), target.name, str(damage))})
+            results.extend(target.fighter.take_damage(damage))
         else:
-            print('{0} attacks {1} but does no damage.'.format(self.owner.name.capitalize(), target.name.capitalize()))
+            results.append({'message': '{0} attacks {1} but does no damage.'.format(
+                self.owner.name.capitalize(), target.name)})
+        
+        return results
